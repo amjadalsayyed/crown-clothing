f3cf88a2145aef80f435c1a9e141d43748d56f33
amjadalsayyed <amgad.alsayyed@yahoo.com>
2022-08-27 17:43:15 +0200

    Directory Page

---
+++ src/component/Directory/Directory.jsx
@@ -0,0 +1,16 @@
+import "./Directory.styles.scss";
+import CategoryItem from "../categor-item/CategoryItem";
+
+import React from "react";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
